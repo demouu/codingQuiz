afafbb1582f2890f2957c764c917523a733e216b
Jack <jckkempton@gmail.com>
2020-10-06 19:38:58 -0600

    Added last questions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
+// Declaring Variables and Grabbing
 var mainEl = document.getElementById("main");
 var question1El = document.getElementById("question1");
 var question2El = document.getElementById("question2");
+var question3El = document.getElementById("question3");
+var question4El = document.getElementById("question4");
+var question5El = document.getElementById("question5");
 function startUp() {
     // 1st Question displayed as empty text.
     question1El.innerHTML = '';
@@ -21,7 +25,7 @@ function startUp() {
     `
     mainEl.innerHTML = mainContent;
     // JQuery Click event listener to start the quiz
-    $("#startQuiz").on("click", function(){
+    $("#startQuiz").on("click", function () {
         // Upon Quiz Start, mainEl disappears and question1El Appears.
         mainEl.innerHTML = '';
         // Question 1 Template Literal
@@ -49,11 +53,11 @@ function startUp() {
         </div>
     </div>
     `
-    question1El.innerHTML = question1;
-    $(".questionList").on("click", function(event){
-        if (event.target.matches("button")){
-            question1El.innerHTML = '';
-            var question2 = `
+        question1El.innerHTML = question1;
+        $(".questionList").on("click", function (event) {
+            if (event.target.matches("button")) {
+                question1El.innerHTML = '';
+                var question2 = `
             <div class="container">
             <div class="card">
                 <div class="card-body">
@@ -77,14 +81,99 @@ function startUp() {
             </div>
         </div>
         `
-        question2El.innerHTML = question2;
-        $(".questionList").on("click", function(event){
-            if (event.target.matches("button")){
-                question2El.innerHTML = '';
+                question2El.innerHTML = question2;
+                $(".questionList").on("click", function (event) {
+                    if (event.target.matches("button")) {
+                        question2El.innerHTML = '';
+                        var question3 = `
+                <div class="container">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Question 3</h4>
+                        <p class="card-text">Arrays in JavaScript can be used to store ____.</p>
+                        <ul class="questionList">
+                            <li>
+                                <button id="q3A1">1. Numbers and Strings</button>
+                            </li>
+                            <li>
+                                <button id="q3A2">2. Other Arrays</button>
+                            </li>
+                            <li>
+                                <button id="q3A3">3. Booleans</button>
+                            </li>
+                            <li>
+                                <button id="q3A4">4. All of the Above</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            `
+                        question3El.innerHTML = question3;
+                        $(".questionList").on("click", function (event) {
+                            if (event.target.matches("button")) {
+                                question3El.innerHTML = '';
+                                var question4 = `
+                                <div class="container">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Question 4</h4>
+                                        <p class="card-text">String values must be enclosed within ____ when being assigned to variables.</p>
+                                        <ul class="questionList">
+                                            <li>
+                                                <button id="q4A1">1. Commas</button>
+                                            </li>
+                                            <li>
+                                                <button id="q4A2">2. Curly Brackets</button>
+                                            </li>
+                                            <li>
+                                                <button id="q4A3">3. Quotes</button>
+                                            </li>
+                                            <li>
+                                                <button id="q4A4">4. Parentheses</button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            `
+                                question4El.innerHTML = question4;
+                                $(".questionList").on("click", function (event) {
+                                    if (event.target.matches("button")) {
+                                        question4El.innerHTML = '';
+                                        var question5 = `
+                                    <div class="container">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h4 class="card-title">Question 5</h4>
+                                            <p class="card-text">A very useful tool during development and debugging for printing content to the debugger is:</p>
+                                            <ul class="questionList">
+                                                <li>
+                                                    <button id="q5A1">1. JavaScript</button>
+                                                </li>
+                                                <li>
+                                                    <button id="q5A2">2. For Loops</button>
+                                                </li>
+                                                <li>
+                                                    <button id="q5A3">3. Terminal / Bash</button>
+                                                </li>
+                                                <li>
+                                                    <button id="q5A4">4. console.log</button>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                `
+                                        question5El.innerHTML = question5;
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
             }
         })
-        }
-    })
     })
 }
 startUp();
