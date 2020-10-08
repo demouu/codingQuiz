289e6dab7107200c3d82143701631fe41273b5d6
Jack <jckkempton@gmail.com>
2020-10-08 14:48:45 -0600

    removed redundant code and rewrote to make it clean and dynamic

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,93 @@ var question3El = document.getElementById("question3");
 var question4El = document.getElementById("question4");
 var question5El = document.getElementById("question5");
 var endEl = document.getElementById("end");
+var question = 0;
+var questionArray = [
+    {
+        question: "The condition within an if / else statement is enclosed within ____.",
+        answers: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        correctA: "Curly Brackets"
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: ["Numbers and Strings", "Booleans", "Other Arrays", "All of the Above"],
+        correctA: "All of the Above"
+    },
+
+    {
+        question: "",
+        answers: [],
+        correctA: ""
+    },
+    {
+        question: "",
+        answers: [],
+        correctA: ""
+    },
+    {
+        question: "",
+        answers: [],
+        correctA: ""
+    }
+
+]
+function nextQuestion(x) {
+    var htmlRender = `
+        <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Question ${question + 1}</h4>
+                <p class="card-text">${questionArray[x].question}</p>
+                <ul id="questionList">
+                    <li>
+                        <button data-answer = "${questionArray[x].answers[0]}">${questionArray[x].answers[0]}</button>
+                    </li>
+                    <li>
+                        <button data-answer = "${questionArray[x].answers[1]}">${questionArray[x].answers[1]}</button>
+                    </li>
+                    <li>
+                        <button data-answer = "${questionArray[x].answers[2]}">${questionArray[x].answers[2]}</button>
+                    </li>
+                    <li>
+                        <button data-answer = "${questionArray[x].answers[3]}">${questionArray[x].answers[3]}</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `
+    mainEl.innerHTML = htmlRender;
+    document.getElementById("questionList").addEventListener("click", function (event) {
+        if (event.target.matches("button")) {
+            var clickedValue = event.target.getAttribute("data-answer");
+            console.log("Clicked Value: " + clickedValue);
+            console.log("Correct Answer: " + questionArray[x].correctA);
+            console.log(event.target);
+            if (clickedValue === questionArray[x].correctA) {
+                console.log("correct");
+            } else {
+                console.log("false");
+                // Decrease timer
+            }
+
+            if (question !== questionArray.length - 1) {
+                question++;
+                nextQuestion(question);
+            } else if (question === questionArray.length - 1) {
+                //Enter initials; Navigate to highscore screen
+            }
+            console.log("Question: " + question);
+            console.log("Question Array Length: " + questionArray.length);
+        }
+    })
+}
+
 function startUp() {
     // 1st Question displayed as empty text.
-    question1El.innerHTML = '';
+    // question1El.innerHTML = '';
     // 2nd Question displayed as empty text.
-    question2El.innerHTML = '';
+    // question2El.innerHTML = '';
     // Utilizing Template Literals for dynamic HTML elements
     var mainContent = `
     <div class="container">
@@ -25,175 +107,9 @@ function startUp() {
     </div>
     `
     mainEl.innerHTML = mainContent;
-    // JQuery Click event listener to start the quiz
-    $("#startQuiz").on("click", function () {
-        // Upon Quiz Start, mainEl disappears and question1El Appears.
-        mainEl.innerHTML = '';
-        // Question 1 Template Literal
-        var question1 = `
-        <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">Question 1</h4>
-                <p class="card-text">Commonly used data types do NOT include:</p>
-                <ul class="questionList">
-                    <li>
-                        <button id="q1A1">1. Strings</button>
-                    </li>
-                    <li>
-                        <button id="q1A2">2. Booleans</button>
-                    </li>
-                    <li>
-                        <button id="q1A3">3. Alerts</button>
-                    </li>
-                    <li>
-                        <button id="q1A4">4. Numbers</button>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    `
-        question1El.innerHTML = question1;
-        $(".questionList").on("click", function (event) {
-            if (event.target.matches("button")) {
-                question1El.innerHTML = '';
-                var question2 = `
-            <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title">Question 2</h4>
-                    <p class="card-text">The condition within an if / else statement is enclosed within ____.</p>
-                    <ul class="questionList">
-                        <li>
-                            <button id="q2A1">1. Quotes</button>
-                        </li>
-                        <li>
-                            <button id="q2A2">2. Curly Brackets</button>
-                        </li>
-                        <li>
-                            <button id="q2A3">3. Parentheses</button>
-                        </li>
-                        <li>
-                            <button id="q2A4">4. Square Brackets</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        `
-                question2El.innerHTML = question2;
-                $(".questionList").on("click", function (event) {
-                    if (event.target.matches("button")) {
-                        question2El.innerHTML = '';
-                        var question3 = `
-                <div class="container">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Question 3</h4>
-                        <p class="card-text">Arrays in JavaScript can be used to store ____.</p>
-                        <ul class="questionList">
-                            <li>
-                                <button id="q3A1">1. Numbers and Strings</button>
-                            </li>
-                            <li>
-                                <button id="q3A2">2. Other Arrays</button>
-                            </li>
-                            <li>
-                                <button id="q3A3">3. Booleans</button>
-                            </li>
-                            <li>
-                                <button id="q3A4">4. All of the Above</button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            `
-                        question3El.innerHTML = question3;
-                        $(".questionList").on("click", function (event) {
-                            if (event.target.matches("button")) {
-                                question3El.innerHTML = '';
-                                var question4 = `
-                                <div class="container">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h4 class="card-title">Question 4</h4>
-                                        <p class="card-text">String values must be enclosed within ____ when being assigned to variables.</p>
-                                        <ul class="questionList">
-                                            <li>
-                                                <button id="q4A1">1. Commas</button>
-                                            </li>
-                                            <li>
-                                                <button id="q4A2">2. Curly Brackets</button>
-                                            </li>
-                                            <li>
-                                                <button id="q4A3">3. Quotes</button>
-                                            </li>
-                                            <li>
-                                                <button id="q4A4">4. Parentheses</button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            `
-                                question4El.innerHTML = question4;
-                                $(".questionList").on("click", function (event) {
-                                    if (event.target.matches("button")) {
-                                        question4El.innerHTML = '';
-                                        var question5 = `
-                                    <div class="container">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <h4 class="card-title">Question 5</h4>
-                                            <p class="card-text">A very useful tool during development and debugging for printing content to the debugger is:</p>
-                                            <ul class="questionList">
-                                                <li>
-                                                    <button id="q5A1">1. JavaScript</button>
-                                                </li>
-                                                <li>
-                                                    <button id="q5A2">2. For Loops</button>
-                                                </li>
-                                                <li>
-                                                    <button id="q5A3">3. Terminal / Bash</button>
-                                                </li>
-                                                <li>
-                                                    <button id="q5A4">4. console.log</button>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                `
-                                        question5El.innerHTML = question5;
-                                        $(".questionList").on("click", function (event){
-                                            if (event.target.matches("button")){
-                                                question5El.innerHTML = '';
-                                                var end = `
-                                                <div class="container">
-                                                <div class="jumbotron-fluid">
-                                                    <h1 class="display-3">Complete!</h1>
-                                                    <p class="lead">That's all folks!</p>
-                                                    <hr class="my-2">
-                                                    <p class="lead">
-                                                    <input type="text"/>
-                                                    <input class="btn btn-primary" type="submit" value="Submit Score">
-                                                    </p>
-                                                </div>
-                                                </div>
-                                                `
-                                                endEl.innerHTML = end;
-                                            }
-                                        })
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
-            }
-        })
+    document.getElementById("startQuiz").addEventListener("click", function () {
+        mainContent.innerHTML = '';
+        nextQuestion(question);
     })
 }
 startUp();
@@ -210,3 +126,5 @@ startUp();
 // Questions I have
 // 1. Template literals. What is a template literal? How to use template literals?
 // 2. 
+
+//
